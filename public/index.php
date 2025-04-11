45c67d6bb036a8375c701af8f3e48e0718ae1d8b
zxshhhh <zhoulopagalan13@gmail.com>
2025-04-11 19:03:39 +0800

    Updated index.php

--- public/index.php
+++ public/index.php
@@ -11,7 +11,9 @@
 |
 */
 
-$app = require __DIR__.'/../bootstrap/app.php';
+require_once __DIR__.'/../vendor/autoload.php';
+
+$app = require_once __DIR__.'/../bootstrap/app.php';
 
 /*
 |--------------------------------------------------------------------------
